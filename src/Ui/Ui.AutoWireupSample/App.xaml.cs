cf5e4a7a9f9bcea19d59d571e8c3204c748bcbf0
Alexander Schmidt <32797407+devdeer-alex@users.noreply.github.com>
2018-01-03 14:21:09 +0100

    Added a sample for auto-wireup. (#34)
    resolves #32

---
+++ src/Ui/Ui.AutoWireupSample/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace devdeer.DoctorFlox.Ui.AutoWireupSample
+{
+    using System;
+    using System.Linq;
+    using System.Windows;
+
+    using Logic.Wpf.Locators;
+
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        public App()
+        {            
+        }
+
+    }
+}
---
+++ src/Ui/Ui.AutoWireupSample/ViewModels/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿namespace devdeer.DoctorFlox.Ui.AutoWireupSample.ViewModels
+{
+    using System;
+    using System.Linq;
+
+    using Logic.Wpf;
+    using Logic.Wpf.Locators;
+
+    using Views;
+
+    /// <summary>
+    /// The view model for the <see cref="MainView" />.
+    /// </summary>
+    /// <remarks>
+    /// This should be automatically resolved as the DataContext for the main view because
+    /// of the naming we use here. The <see cref="AutoViewModelLocator" /> is bound in the MainView.xaml
+    /// and will search for a MainViewModel if the window is called MainView. The parts 'ViewModel' and 'View'
+    /// in those names are key.
+    /// </remarks>
+    public class MainViewModel : BaseViewModel
+    {
+        #region properties
+
+        /// <summary>
+        /// The caption for the view.
+        /// </summary>
+        public string Caption { get; set; } = "Autowire";
+
+        #endregion
+    }
+}

6e83e9776c7ba5e2916612d4499bdae7b409f99a
Alexander Schmidt <alexander.schmidt@devdeer.com>
2018-01-03 16:24:01 +0100

    Updated assemblyinfos and synched with nuget-version.

--- src/Logic/Logic.Wpf/Properties/AssemblyInfo.cs
+++ src/Logic/Logic.Wpf/Properties/AssemblyInfo.cs
@@ -5,6 +5,6 @@
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("DEVDEER")]
 [assembly: AssemblyProduct("DoctorFlox")]
-[assembly: AssemblyCopyright("Copyright © DEVDEER 2017")]
+[assembly: AssemblyCopyright("Copyright © DEVDEER 2017-2018")]
 [assembly: AssemblyTrademark("DoctorFlox")]
-[assembly: AssemblyVersion("0.2.2.*")]
+[assembly: AssemblyVersion("0.2.4.*")]

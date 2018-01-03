061a85a17b8319f2413f46182f135b52eee710ff
Alexander Schmidt <alexander.schmidt@devdeer.com>
2018-01-03 14:10:17 +0100

    Added a sample for auto-wireup. resolves #32

---
+++ src/Ui/Ui.AutoWireupSample/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("DoctorFlox.Ui.AutoWireupSample")]
+[assembly: AssemblyDescription(".NET WPF application for testing the auto-wireup of view models without .")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("DEVDEER")]
+[assembly: AssemblyProduct("DoctorFlox")]
+[assembly: AssemblyCopyright("Copyright © DEVDEER 2017-2018")]
+[assembly: AssemblyTrademark("DoctorFlox")]
+[assembly: AssemblyVersion("1.0.1.*")]
---
+++ src/Ui/Ui.AutoWireupSample/Views/MainView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace devdeer.DoctorFlox.Ui.AutoWireupSample.Views
+{
+    using System;
+    using System.Linq;
+    using System.Windows;
+
+    using Logic.Wpf.Locators;
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainView : Window
+    {
+        #region constructors and destructors
+
+        public MainView()
+        {
+            InitializeComponent();            
+        }
+
+        #endregion
+    }
+}

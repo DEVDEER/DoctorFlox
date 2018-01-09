ee6bf2dbfbf0fd5bab93a1c5a9fb45f09db480f5
Alexander Schmidt <32797407+devdeer-alex@users.noreply.github.com>
2018-01-09 14:46:13 +0100

    Updated assemblyinfos and synched with nuget-version. (#35)

--- src/Ui/Ui.WpfSample/Properties/AssemblyInfo.cs
+++ src/Ui/Ui.WpfSample/Properties/AssemblyInfo.cs
@@ -5,6 +5,6 @@
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("DEVDEER")]
 [assembly: AssemblyProduct("DoctorFlox")]
-[assembly: AssemblyCopyright("Copyright © DEVDEER 2017")]
+[assembly: AssemblyCopyright("Copyright © DEVDEER 2017-2018")]
 [assembly: AssemblyTrademark("DoctorFlox")]
-[assembly: AssemblyVersion("0.0.1.*")]
+[assembly: AssemblyVersion("1.0.1.*")]

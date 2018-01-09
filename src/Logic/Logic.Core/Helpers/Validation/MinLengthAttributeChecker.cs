ef410a844bfc2a5409740662a723432d04779bc4
Alexander Schmidt <alexander.schmidt@devdeer.com>
2018-01-09 14:57:40 +0100

    All items copied from Logic.Wpf to Logic.Core and namespaces refactored.

---
+++ src/Logic/Logic.Core/Helpers/Validation/MinLengthAttributeChecker.cs
@@ -0,0 +1,30 @@
+﻿namespace devdeer.DoctorFlox.Helpers.Validation
+{
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
+
+    using Extensions;
+
+    using Models;
+
+    /// <summary>
+    /// Validation checker for <see cref="MinLengthAttribute" />.
+    /// </summary>
+    public class MinLengthAttributeChecker : BaseAttributeChecker<MinLengthAttribute>
+    {
+        #region methods
+
+        /// <inheritdoc />
+        protected override ValidationAttributeCheckResult InternalCheck(MinLengthAttribute attribute, object currentValue)
+        {
+            if ((currentValue?.ToString().Length ?? 0) < attribute.Length)
+            {
+                return new ValidationAttributeCheckResult(false, attribute.ResolveErrorMessage());
+            }
+            return new ValidationAttributeCheckResult();
+        }
+
+        #endregion
+    }
+}
---
+++ src/Logic/Logic.Core/Interfaces/ICleanup.cs
@@ -0,0 +1,23 @@
+﻿namespace devdeer.DoctorFlox.Interfaces
+{
+    using System;
+    using System.Linq;
+
+    /// <summary>
+    /// Defines a common interface for classes that should be cleaned up,
+    /// but without the implications that IDisposable presupposes. An instance
+    /// implementing ICleanup can be cleaned up without being
+    /// disposed and garbage collected.
+    /// </summary>
+    public interface ICleanup
+    {
+        #region methods
+
+        /// <summary>
+        /// Cleans up the instance, for example by saving its state, removing resources, etc..
+        /// </summary>
+        void Cleanup();
+
+        #endregion
+    }
+}

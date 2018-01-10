3b7e6f717dce7b2ad125d53e49084b10f45d2b07
Alexander Schmidt <alexander.schmidt@devdeer.com>
2018-01-10 14:53:49 +0100

    Added unit test for ValidationAttributeCheckResult resolves #50

---
+++ src/Tests/Tests.Logic.Core/ValidationAttributeCheckResultTests.cs
@@ -0,0 +1,40 @@
+﻿namespace deveer.DoctorFlox.Tests.Logic.Core
+{
+    using System;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Linq;
+
+    using devdeer.DoctorFlox.Models;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    /// <summary>
+    /// Contains unit tests for the type <see cref="ValidationAttributeCheckResult" />.
+    /// </summary>
+    [TestClass]
+    [ExcludeFromCodeCoverage]
+    public class ValidationAttributeCheckResultTests
+    {
+        #region methods
+
+        /// <summary>
+        /// Tests if the constructor will store the provided values correctly.
+        /// </summary>
+        [TestMethod]
+        public void ValidationAttributeCheckResultConstructorTest()
+        {
+            var random = new Random(DateTime.Now.Millisecond);
+            for (var i = 0; i < 1000; i++)
+            {
+                var desiredLength = random.Next(1, 20000);
+                var errorMessage = new string('c', desiredLength);
+                var item = new ValidationAttributeCheckResult(i % 10 == 0, errorMessage);
+                Assert.AreEqual(i % 10 == 0, item.IsValid);
+                Assert.IsNotNull(item.ErrorMessage);
+                Assert.AreEqual(desiredLength, item.ErrorMessage.Length);
+            }
+        }
+
+        #endregion
+    }
+}
